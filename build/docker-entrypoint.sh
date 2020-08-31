798dbf340877a638871a78d8f4785de50dc0e313
WoosungChoi <solut2000@gmail.com>
2020-08-31 11:23:24 +0900

    add utf8mb4

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -10,6 +10,7 @@ mkdir gnuboard \
 && rm gnuboard.tar.gz && mkdir data \
 && chown -R www-data:www-data ./ \
 && chmod -R 777 data \
+&& sed -i "s/'utf8'/'utf8mb4'/g" config.php \
 && cd ..
 fi
 # 그누보드 자동설치 스크립트 끝
