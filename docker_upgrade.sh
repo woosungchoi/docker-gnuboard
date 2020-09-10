88684d23265c49388c805328b3cfc7681e94a01a
WoosungChoi <solut2000@gmail.com>
2020-09-10 22:09:16 +0900

    fix for upgrade

--- docker_upgrade.sh
+++ docker_upgrade.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /your/path/docker-multi-site/
+cd /your/path/to/docker-gnuboard/
 
 $COMPOSE pull && $DOCKER pull woosungchoi/fpm-alpine && $COMPOSE up --build -d && $DOCKER image prune -f
